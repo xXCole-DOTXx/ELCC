596cdb7cd429e7ddc6fac57d553efddff016f0df
Cole Perry <E096752@wv.gov>
2021-10-12 14:29:33 -0400

    Push before sorting and pagination

--- EnvApp/Controllers/Project_ScreenController.cs
+++ EnvApp/Controllers/Project_ScreenController.cs
@@ -71,6 +71,66 @@ namespace EnvApp.Controllers
             };
             options.Insert(0, new SelectListItem { Value = "Select" });
             ViewBag.options = options;
+
+            List<SelectListItem> counties = new()
+            {
+                new SelectListItem { Value = "Barbour", Text = "Barbour County" },
+                new SelectListItem { Value = "Berkeley", Text = "Berkeley County" },
+                new SelectListItem { Value = "Boone", Text = "Boone County" },
+                new SelectListItem { Value = "Braxton", Text = "Braxton County" },
+                new SelectListItem { Value = "Cabell", Text = "Cabell County" },
+                new SelectListItem { Value = "Calhoun", Text = "Calhoun County" },
+                new SelectListItem { Value = "Clay", Text = "Clay County" },
+                new SelectListItem { Value = "Doddridge", Text = "Doddridge County" },
+                new SelectListItem { Value = "Fayette", Text = "Fayette County" },
+                new SelectListItem { Value = "Gilmer", Text = "Gilmer County" },
+                new SelectListItem { Value = "Grant", Text = "Grant County" },
+                new SelectListItem { Value = "Greenbrier", Text = "Greenbrier County" },
+                new SelectListItem { Value = "Hampshire", Text = "Hampshire County" },
+                new SelectListItem { Value = "Hancock", Text = "Hancock County" },
+                new SelectListItem { Value = "Hardy", Text = "Hardy County" },
+                new SelectListItem { Value = "Harrison", Text = "Harrison County" },
+                new SelectListItem { Value = "Jackson", Text = "Jackson County" },
+                new SelectListItem { Value = "Jefferson", Text = "Jefferson County" },
+                new SelectListItem { Value = "Kanawha", Text = "Kanawha County" },
+                new SelectListItem { Value = "Lewis", Text = "Lewis County" },
+                new SelectListItem { Value = "Lincoln", Text = "Lincoln County" },
+                new SelectListItem { Value = "Logan", Text = "Logan County" },
+                new SelectListItem { Value = "Marion", Text = "Marion County" },
+                new SelectListItem { Value = "Marshall", Text = "Marshall County" },
+                new SelectListItem { Value = "Mason", Text = "Mason County" },
+                new SelectListItem { Value = "McDowell", Text = "McDowell County" },
+                new SelectListItem { Value = "Mercer", Text = "Mercer County" },
+                new SelectListItem { Value = "Mineral", Text = "Mineral County" },
+                new SelectListItem { Value = "Mingo", Text = "Mingo County" },
+                new SelectListItem { Value = "Monongalia", Text = "Monongalia County" },
+                new SelectListItem { Value = "Monroe", Text = "Monroe County" },
+                new SelectListItem { Value = "Morgan", Text = "Morgan County" },
+                new SelectListItem { Value = "Nicholas", Text = "Nicholas County" },
+                new SelectListItem { Value = "Ohio", Text = "Ohio County" },
+                new SelectListItem { Value = "Pendleton", Text = "Pendleton County" },
+                new SelectListItem { Value = "Pleasants", Text = "Pleasants County" },
+                new SelectListItem { Value = "Pocahontas", Text = "Pocahontas County" },
+                new SelectListItem { Value = "Preston", Text = "Preston County" },
+                new SelectListItem { Value = "Putnam", Text = "Putnam County" },
+                new SelectListItem { Value = "Raleigh", Text = "Raleigh County" },
+                new SelectListItem { Value = "Randolph", Text = "Randolph County" },
+                new SelectListItem { Value = "Ritchie", Text = "Ritchie County" },
+                new SelectListItem { Value = "Roane", Text = "Roane County" },
+                new SelectListItem { Value = "Summers", Text = "Summers County" },
+                new SelectListItem { Value = "Taylor", Text = "Taylor County" },
+                new SelectListItem { Value = "Tucker", Text = "Tucker County" },
+                new SelectListItem { Value = "Tyler", Text = "Tyler County" },
+                new SelectListItem { Value = "Upshur", Text = "Upshur County" },
+                new SelectListItem { Value = "Wayne", Text = "Wayne County" },
+                new SelectListItem { Value = "Webster", Text = "Webster County" },
+                new SelectListItem { Value = "Wetzel", Text = "Wetzel County" },
+                new SelectListItem { Value = "Wirt", Text = "Wirt County" },
+                new SelectListItem { Value = "Wood", Text = "Wood County" },
+                new SelectListItem { Value = "Wyoming", Text = "Wyoming County" }
+            };
+            ViewBag.counties = counties;
+
             return View();
         }
 
--- EnvApp/Models/DB/Project_Screen.cs
+++ EnvApp/Models/DB/Project_Screen.cs
@@ -15,18 +15,18 @@ namespace EnvApp.Models.DB
         [Key]
         public long ID { get; set; }
         public string State_Project_Number { get; set; }
-        public string Federal_Project_Number { get; set; }
+        public string? Federal_Project_Number { get; set; }
         public string Project_Name { get; set; }
         public string County { get; set; }
         public DateTime? Memo_Date { get; set; }
         public string From { get; set; }
-        public string Authorization { get; set; }
+        public string? Authorization { get; set; }
         public string DSN_PM { get; set; }
-        public string History { get; set; }
+        public string? History { get; set; }
         public string History_PM { get; set; }
         public bool Review_Exempt_H { get; set; }
         public bool SHPO_Approval_H { get; set; }
-        public string Archaeology { get; set; }
+        public string? Archaeology { get; set; }
         public string Archaeology_PM { get; set; }
         public bool Review_Exempt_A { get; set; }
         public bool SHPO_Approval_A { get; set; }
@@ -38,12 +38,12 @@ namespace EnvApp.Models.DB
         public string USFWS_Type { get; set; }
         public bool Mussel_Habitat { get; set; }
         public bool Mussel_Stream { get; set; }
-        public string Mussel_Stream_Survey { get; set; }
+        public string? Mussel_Stream_Survey { get; set; }
         public string Within_Airport { get; set; }
         public string? ToPo_Quad_Name { get; set; }
         public bool Bat_Habitat { get; set; }
-        public string Bars { get; set; }
-        public float? Coordinates { get; set; }
+        public string? Bars { get; set; }
+        public string Coordinates { get; set; }
         public string? Natural_Resources_Notes { get; set; }
         public string Adduser { get; set; }
         public DateTime? Date_Added { get; set; }
