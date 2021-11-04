12d379be0c80814416fbc7933ab7ec7676ac02c7
xXCole-DOTXx <cole.k.perry@wv.gov>
2021-11-04 15:41:39 -0400

    Abstracted the drop down menus to another method

--- EnvApp/Controllers/TypeOnesController.cs
+++ EnvApp/Controllers/TypeOnesController.cs
@@ -67,92 +67,7 @@ namespace EnvApp.Controllers
         // GET: TypeOnes/Create
         public IActionResult Create()
         {
-            List<string> users = (from c in _context.NR_Users select c.Name).ToList();
-            users.Insert(0, "Select");
-            ViewBag.users = users;
-
-            List<string> adminLeads = (from s in _context.NR_Users
-                                      where s.User_Type == "Admin" || s.User_Type == "Unit Leader"
-                                      select s.Name).ToList();
-            adminLeads.Insert(0, "Select");
-            ViewBag.adminLeads = adminLeads.ToList();
-
-            List<SelectListItem> options = new()
-            {
-                new SelectListItem { Value = "True", Text = "Yes" },
-                new SelectListItem { Value = "False", Text = "No" }
-            };
-            options.Insert(0, new SelectListItem { Value = "Select" });
-            ViewBag.options = options;
-
-            List <SelectListItem> reTypes = new()
-            {
-                new SelectListItem { Value = "Appendix A short form", Text = "Appendix A short form" },
-                new SelectListItem { Value = "Review exempt", Text = "Review exempt" },
-                new SelectListItem { Value = "SHPO", Text = "SHPO" },
-                new SelectListItem { Value = "Programatic Agreement", Text = "Programatic Agreement" }
-            };
-            reTypes.Insert(0, new SelectListItem { Value = "Select", Text = "Select" });
-            ViewBag.reTypes = reTypes;
-
-            List <SelectListItem> counties = new()
-            {
-                new SelectListItem { Value = "Barbour", Text = "Barbour County" },
-                new SelectListItem { Value = "Berkeley", Text = "Berkeley County" },
-                new SelectListItem { Value = "Boone", Text = "Boone County" },
-                new SelectListItem { Value = "Braxton", Text = "Braxton County" },
-                new SelectListItem { Value = "Cabell", Text = "Cabell County" },
-                new SelectListItem { Value = "Calhoun", Text = "Calhoun County" },
-                new SelectListItem { Value = "Clay", Text = "Clay County" },
-                new SelectListItem { Value = "Doddridge", Text = "Doddridge County" },
-                new SelectListItem { Value = "Fayette", Text = "Fayette County" },
-                new SelectListItem { Value = "Gilmer", Text = "Gilmer County" },
-                new SelectListItem { Value = "Grant", Text = "Grant County" },
-                new SelectListItem { Value = "Greenbrier", Text = "Greenbrier County" },
-                new SelectListItem { Value = "Hampshire", Text = "Hampshire County" },
-                new SelectListItem { Value = "Hancock", Text = "Hancock County" },
-                new SelectListItem { Value = "Hardy", Text = "Hardy County" },
-                new SelectListItem { Value = "Harrison", Text = "Harrison County" },
-                new SelectListItem { Value = "Jackson", Text = "Jackson County" },
-                new SelectListItem { Value = "Jefferson", Text = "Jefferson County" },
-                new SelectListItem { Value = "Kanawha", Text = "Kanawha County" },
-                new SelectListItem { Value = "Lewis", Text = "Lewis County" },
-                new SelectListItem { Value = "Lincoln", Text = "Lincoln County" },
-                new SelectListItem { Value = "Logan", Text = "Logan County" },
-                new SelectListItem { Value = "Marion", Text = "Marion County" },
-                new SelectListItem { Value = "Marshall", Text = "Marshall County" },
-                new SelectListItem { Value = "Mason", Text = "Mason County" },
-                new SelectListItem { Value = "McDowell", Text = "McDowell County" },
-                new SelectListItem { Value = "Mercer", Text = "Mercer County" },
-                new SelectListItem { Value = "Mineral", Text = "Mineral County" },
-                new SelectListItem { Value = "Mingo", Text = "Mingo County" },
-                new SelectListItem { Value = "Monongalia", Text = "Monongalia County" },
-                new SelectListItem { Value = "Monroe", Text = "Monroe County" },
-                new SelectListItem { Value = "Morgan", Text = "Morgan County" },
-                new SelectListItem { Value = "Nicholas", Text = "Nicholas County" },
-                new SelectListItem { Value = "Ohio", Text = "Ohio County" },
-                new SelectListItem { Value = "Pendleton", Text = "Pendleton County" },
-                new SelectListItem { Value = "Pleasants", Text = "Pleasants County" },
-                new SelectListItem { Value = "Pocahontas", Text = "Pocahontas County" },
-                new SelectListItem { Value = "Preston", Text = "Preston County" },
-                new SelectListItem { Value = "Putnam", Text = "Putnam County" },
-                new SelectListItem { Value = "Raleigh", Text = "Raleigh County" },
-                new SelectListItem { Value = "Randolph", Text = "Randolph County" },
-                new SelectListItem { Value = "Ritchie", Text = "Ritchie County" },
-                new SelectListItem { Value = "Roane", Text = "Roane County" },
-                new SelectListItem { Value = "Summers", Text = "Summers County" },
-                new SelectListItem { Value = "Taylor", Text = "Taylor County" },
-                new SelectListItem { Value = "Tucker", Text = "Tucker County" },
-                new SelectListItem { Value = "Tyler", Text = "Tyler County" },
-                new SelectListItem { Value = "Upshur", Text = "Upshur County" },
-                new SelectListItem { Value = "Wayne", Text = "Wayne County" },
-                new SelectListItem { Value = "Webster", Text = "Webster County" },
-                new SelectListItem { Value = "Wetzel", Text = "Wetzel County" },
-                new SelectListItem { Value = "Wirt", Text = "Wirt County" },
-                new SelectListItem { Value = "Wood", Text = "Wood County" },
-                new SelectListItem { Value = "Wyoming", Text = "Wyoming County" }
-            };
-            ViewBag.counties = counties;
+            DropDowns();
 
             return View();
         }
@@ -162,19 +77,38 @@ namespace EnvApp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,State_Project_Number,Federal_Project_Number,Name,Route_Number,County,Work_Type,Coordinates,Project_Description,Federal_Aid,Minimal_Project_Verification,CE_Category,Amms,Activities_Agreement,Prepared_By,Approved_By,Arch_RE,Hist_RE,Arch_RE_Date,Hist_RE_Date,Through_Lanes,Close_Road,ROW_Acquisition,Access_Control,Fifty_Year_Structure,Agency_Coordination,IPAC_Screening_Zone,Section_404_Permit,Ground_Disturbance,Waterway,Special_Use_Permit,Floodplain,Adduser,Date_Added")] TypeOne typeOne, bool Assessment)
+        public async Task<IActionResult> Create([Bind("ID,State_Project_Number,Federal_Project_Number,Name,Route_Number,County,Work_Type,Coordinates,Project_Description,Federal_Aid,Minimal_Project_Verification,CE_Category,Amms,Activities_Agreement,Arch_RE,Hist_RE,Arch_RE_Date,Hist_RE_Date,Through_Lanes,Close_Road,ROW_Acquisition,Access_Control,Fifty_Year_Structure,Agency_Coordination,IPAC_Screening_Zone,Section_404_Permit,Ground_Disturbance,Waterway,Special_Use_Permit,Floodplain,Prepared_By,Approved_By,Adduser,Date_Added")] TypeOne typeOne, string Assessment, bool Bat)
         {
+            DropDowns();
+
             System.Diagnostics.Debug.WriteLine("Prepared by: " + typeOne.Prepared_By);
             if (ModelState.IsValid)
             {
                 typeOne.Adduser = User.Identity.Name;
                 typeOne.Date_Added = DateTime.Today;
-                if(Assessment)
+                System.Diagnostics.Debug.WriteLine("Prepared by again: " + typeOne.Prepared_By);
+                var prep = typeOne.Prepared_By;
+                typeOne.Prepared_By = prep;
+                _context.Add(typeOne);
+                await _context.SaveChangesAsync();
+                //Send all History and Archaeology Unit Leaders an email
+                List<string> histAndArchLeads = (from s in _context.NR_Users
+                                           where s.User_Type == "Unit Leader" && s.Unit == "History" || s.Unit == "Archaeology"
+                                           select s.Email_Address).ToList();
+                foreach(var email in histAndArchLeads)
+                {
+                    SendEmail(email);
+                }
+                //Send an email to Traci if project needs a Mussel or Crayfish habitat assessement (Natural resources Lead)
+                if (Assessment != "No" )
                 {
                     SendEmail("Cole.k.perry@wv.gov");
                 }
-                _context.Add(typeOne);
-                await _context.SaveChangesAsync();
+                //Send an email to bat lady if project needs a bat habitat assessement
+                if (Bat)
+                {
+                    SendEmail("Cole.k.perry@wv.gov");
+                }
                 return RedirectToAction(nameof(Index));
             }
             return View(typeOne);
@@ -279,6 +213,106 @@ namespace EnvApp.Controllers
             client1.UseDefaultCredentials = false; // Important: This line of code must be executed before setting the NetworkCredentials object, otherwise the setting will be reset (a bug in .NET)
             NetworkCredential cred1 = new System.Net.NetworkCredential("DOHEnvironmentalAppSrv@wv.gov", "wnC6W6?C"); client1.Credentials = cred1;
             client1.Send(myMail);
+        }
+
+        public void DropDowns()
+        {
+            List<string> users = (from c in _context.NR_Users select c.Name).ToList();
+            users.Insert(0, "Select");
+            ViewBag.users = users;
+
+            List<string> adminLeads = (from s in _context.NR_Users
+                                       where s.User_Type == "Admin" || s.User_Type == "Unit Leader"
+                                       select s.Name).ToList();
+            adminLeads.Insert(0, "Select");
+            ViewBag.adminLeads = adminLeads.ToList();
+
+            List<SelectListItem> options = new()
+            {
+                new SelectListItem { Value = "True", Text = "Yes" },
+                new SelectListItem { Value = "False", Text = "No" }
+            };
+            options.Insert(0, new SelectListItem { Value = "Select" });
+            ViewBag.options = options;
+
+            List<SelectListItem> assessments = new()
+            {
+                new SelectListItem { Value = "Mussel", Text = "Mussel" },
+                new SelectListItem { Value = "Crayfish", Text = "Crayfish" },
+                new SelectListItem { Value = "Both", Text = "Both" },
+                new SelectListItem { Value = "No", Text = "No" }
+            };
+            assessments.Insert(0, new SelectListItem { Value = "Select" });
+            ViewBag.assessments = assessments;
+
+            List<SelectListItem> reTypes = new()
+            {
+                new SelectListItem { Value = "Appendix A short form", Text = "Appendix A short form" },
+                new SelectListItem { Value = "Review exempt", Text = "Review exempt" },
+                new SelectListItem { Value = "SHPO", Text = "SHPO" },
+                new SelectListItem { Value = "Programatic Agreement", Text = "Programatic Agreement" }
+            };
+            reTypes.Insert(0, new SelectListItem { Value = "Select", Text = "Select" });
+            ViewBag.reTypes = reTypes;
+
+            List<SelectListItem> counties = new()
+            {
+                new SelectListItem { Value = "Barbour", Text = "Barbour County" },
+                new SelectListItem { Value = "Berkeley", Text = "Berkeley County" },
+                new SelectListItem { Value = "Boone", Text = "Boone County" },
+                new SelectListItem { Value = "Braxton", Text = "Braxton County" },
+                new SelectListItem { Value = "Cabell", Text = "Cabell County" },
+                new SelectListItem { Value = "Calhoun", Text = "Calhoun County" },
+                new SelectListItem { Value = "Clay", Text = "Clay County" },
+                new SelectListItem { Value = "Doddridge", Text = "Doddridge County" },
+                new SelectListItem { Value = "Fayette", Text = "Fayette County" },
+                new SelectListItem { Value = "Gilmer", Text = "Gilmer County" },
+                new SelectListItem { Value = "Grant", Text = "Grant County" },
+                new SelectListItem { Value = "Greenbrier", Text = "Greenbrier County" },
+                new SelectListItem { Value = "Hampshire", Text = "Hampshire County" },
+                new SelectListItem { Value = "Hancock", Text = "Hancock County" },
+                new SelectListItem { Value = "Hardy", Text = "Hardy County" },
+                new SelectListItem { Value = "Harrison", Text = "Harrison County" },
+                new SelectListItem { Value = "Jackson", Text = "Jackson County" },
+                new SelectListItem { Value = "Jefferson", Text = "Jefferson County" },
+                new SelectListItem { Value = "Kanawha", Text = "Kanawha County" },
+                new SelectListItem { Value = "Lewis", Text = "Lewis County" },
+                new SelectListItem { Value = "Lincoln", Text = "Lincoln County" },
+                new SelectListItem { Value = "Logan", Text = "Logan County" },
+                new SelectListItem { Value = "Marion", Text = "Marion County" },
+                new SelectListItem { Value = "Marshall", Text = "Marshall County" },
+                new SelectListItem { Value = "Mason", Text = "Mason County" },
+                new SelectListItem { Value = "McDowell", Text = "McDowell County" },
+                new SelectListItem { Value = "Mercer", Text = "Mercer County" },
+                new SelectListItem { Value = "Mineral", Text = "Mineral County" },
+                new SelectListItem { Value = "Mingo", Text = "Mingo County" },
+                new SelectListItem { Value = "Monongalia", Text = "Monongalia County" },
+                new SelectListItem { Value = "Monroe", Text = "Monroe County" },
+                new SelectListItem { Value = "Morgan", Text = "Morgan County" },
+                new SelectListItem { Value = "Nicholas", Text = "Nicholas County" },
+                new SelectListItem { Value = "Ohio", Text = "Ohio County" },
+                new SelectListItem { Value = "Pendleton", Text = "Pendleton County" },
+                new SelectListItem { Value = "Pleasants", Text = "Pleasants County" },
+                new SelectListItem { Value = "Pocahontas", Text = "Pocahontas County" },
+                new SelectListItem { Value = "Preston", Text = "Preston County" },
+                new SelectListItem { Value = "Putnam", Text = "Putnam County" },
+                new SelectListItem { Value = "Raleigh", Text = "Raleigh County" },
+                new SelectListItem { Value = "Randolph", Text = "Randolph County" },
+                new SelectListItem { Value = "Ritchie", Text = "Ritchie County" },
+                new SelectListItem { Value = "Roane", Text = "Roane County" },
+                new SelectListItem { Value = "Summers", Text = "Summers County" },
+                new SelectListItem { Value = "Taylor", Text = "Taylor County" },
+                new SelectListItem { Value = "Tucker", Text = "Tucker County" },
+                new SelectListItem { Value = "Tyler", Text = "Tyler County" },
+                new SelectListItem { Value = "Upshur", Text = "Upshur County" },
+                new SelectListItem { Value = "Wayne", Text = "Wayne County" },
+                new SelectListItem { Value = "Webster", Text = "Webster County" },
+                new SelectListItem { Value = "Wetzel", Text = "Wetzel County" },
+                new SelectListItem { Value = "Wirt", Text = "Wirt County" },
+                new SelectListItem { Value = "Wood", Text = "Wood County" },
+                new SelectListItem { Value = "Wyoming", Text = "Wyoming County" }
+            };
+            ViewBag.counties = counties;
         }
     }
 }
