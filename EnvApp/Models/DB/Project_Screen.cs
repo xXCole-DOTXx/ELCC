7d95a615304f1af49dea93d80fa8782e09468ccb
Cole Perry <E096752@wv.gov>
2021-11-16 12:04:00 -0500

    Updated to Project_Screen model and create

--- EnvApp/Models/DB/Project_Screen.cs
+++ EnvApp/Models/DB/Project_Screen.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using ExpressiveAnnotations.Attributes;
 
 #nullable disable
 
@@ -10,13 +11,19 @@ namespace EnvApp.Models.DB
     {
         [Key]
         public long ID { get; set; }
+        [MaxLength(50)]
         public string State_Project_Number { get; set; }
+        [MaxLength(50)]
         public string? Federal_Project_Number { get; set; }
+        [MaxLength(50)]
         public string Project_Name { get; set; }
         public string County { get; set; }
         public DateTime? Memo_Date { get; set; }
+        [MaxLength(50)]
         public string From { get; set; }
+        [MaxLength(50)]
         public string? Authorization { get; set; }
+        [MaxLength(50)]
         public string DSN_PM { get; set; }
         public string? History { get; set; }
         public string History_PM { get; set; }
@@ -31,19 +38,29 @@ namespace EnvApp.Models.DB
         public bool Crayfish_Habitat_Assessment { get; set; }
         public bool NLEB_4D { get; set; }
         public bool USFWS { get; set; }
+        [RequiredIf("USFWS == true", ErrorMessage = "USFWS Type is required.")]
         public string USFWS_Type { get; set; }
         public bool Mussel_Habitat { get; set; }
         public bool Mussel_Stream { get; set; }
+        [MaxLength(100)]
         public string Within_Airport { get; set; }
+        [MaxLength(100)]
         public string? ToPo_Quad_Name { get; set; }
         public bool Bat_Habitat { get; set; }
+        [MaxLength(6)]
         public string? Bars { get; set; }
+        [MaxLength(100)]
         public string Coordinates { get; set; }
         public string? Natural_Resources_Notes { get; set; }
+        [MaxLength(20)]
         public string Adduser { get; set; }
         public DateTime? Date_Added { get; set; }
         public string Crayfish_Notes { get; set; }
         public string Mussel_Notes { get; set; }
 
+    }
+    public class MyCustomAttribute : Attribute
+    {
+        public string SomeProperty { get; set; }
     }
 }
